524bed3a0d2eeaf8f601f2235fd2f6c5b50e3b64
toran billups <toranb@gmail.com>
2015-03-12 21:58:44 -0500

    much simpler setup for big apps that inject repos

--- app/initializers/repositories.js
+++ app/initializers/repositories.js
@@ -1,8 +1,9 @@
+import Ember from "ember";
 import PersonRepository from "example/repositories/person";
 
 export function initialize(container, application) {
     application.register("repositories:person", PersonRepository);
-    application.inject("repositories:person", "store", "store:main");
+    application.inject("repositories", "store", "store:main");
     Ember.inject.repositories = function(name) {
         return container.lookup("repositories:" + name);
     };
