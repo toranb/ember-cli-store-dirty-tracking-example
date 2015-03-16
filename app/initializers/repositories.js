e11b6df6967d92edca62a21e40fa9e9e6b521aff
toran billups <toranb@gmail.com>
2015-03-15 19:37:57 -0500

    now using auto-register

--- app/initializers/repositories.js
+++ app/initializers/repositories.js
@@ -1,8 +1,8 @@
 import Ember from "ember";
-import PersonRepository from "example/repositories/person";
+import registerWithContainer from "ember-cli-auto-register/register";
 
 export function initialize(container, application) {
-    application.register("repositories:person", PersonRepository);
+    registerWithContainer("repositories", application);
     application.inject("repositories", "store", "store:main");
     Ember.inject.repositories = function(name) {
         return container.lookup("repositories:" + name);
